94ad2be24011b2ef31c5a8de893172dc56252b3e
Alex Balaguer <balaguer.dev@gmail.com>
2022-08-10 17:04:21 +0200

    push1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let contain = document.querySelector(".container")
-
-
+let titlePost
+let titleModal = document.querySelector(".titleModal")
+/* MAIN CONTAINER */
+/* MAIN CONTAINER */
 fetch("http://localhost:3000/posts")
 .then((res)=> res.json())
 .then((posts)=>{
@@ -10,32 +12,57 @@ fetch("http://localhost:3000/posts")
         divPost.setAttribute("data-bs-target", "#exampleModal")
         
         /* crear elementos */
-        let titlePost = document.createElement("h2")
+        titlePost = document.createElement("h2")
         titlePost.setAttribute("class", "postTitle")
+        titlePost.setAttribute("class", "modal-title")
+
         titlePost.appendChild(
+            document.createTextNode(`${post.title}`))
+            
+        titleModal.appendChild(
             document.createTextNode(`${post.title}`)
+        
         )
-
-
-        let bodyPost = document.createElement("p")
+        
+ /*        let bodyPost = document.createElement("p")
         bodyPost.setAttribute("class", "postBody")
         bodyPost.appendChild(
             document.createTextNode(`${post.body}`)
-        )
-
+        ) */
         let separationPost = document.createElement("hr")
-
-        
 
         /* asignar elementos a sus padres */
         divPost.appendChild(titlePost)
-        divPost.appendChild(bodyPost)
+       /*  divPost.appendChild(bodyPost) */
         divPost.appendChild(separationPost)
         contain.appendChild(divPost)
     });
-   
-    
 })
 
-/* setAtribute   para crear atributosd*/
 
+
+/* MODAL POST */
+/* MODAL POST */
+
+
+let bodyModalPost = document.querySelector(".bodyModalPost")
+let titleModalUser =document.querySelector(".titleModalUser")
+let userNameModal =document.querySelector(".userNameModal")
+let emailModal =document.querySelector(".emailModal")
+let btnComments = document.querySelector(".btn-dark")
+
+/* 
+fetch("http://localhost:3000/posts")
+.then((res)=> res.json())
+.then((postsM)=>{
+    postsM.forEach(pM => {
+        let titleText= document.createTextNode(`${pM.title}`)
+            
+        if(titlePost===titleText){
+            titleModal.appendChild(titleText) 
+        }
+    });
+    console.log(titlePost)
+})
+    
+ */

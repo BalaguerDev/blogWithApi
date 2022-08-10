f2e4e7bb2a318e14b236a39706281ed7741e2102
Alex Balaguer <balaguer.dev@gmail.com>
2022-08-10 12:21:15 +0200

    push

--- script.js
+++ script.js
@@ -8,31 +8,25 @@ fetch("http://localhost:3000/posts")
         /* crear elementos */
         let listPost = document.createElement("li")
 
-        let titlePost = document.createElement("h2")
-        titlePost.appendChild(
-            document.createTextNode(`${post.title}`)
-        )
+        
 
         let bodyPost = document.createElement("p")
         bodyPost.appendChild(
             document.createTextNode(`${post.body}`)
         )
-        
+        let titlePost = document.createElement("h2")
+        titlePost.appendChild(
+            document.createTextNode(`${post.title}`)
+        )
+
         /* asignar elementos a sus padres */
-        listPost.appendChild(bodyPost)
         listPost.appendChild(titlePost)
+        listPost.appendChild(bodyPost)
+        
         ulPost.appendChild(listPost)
     });
     infoPosts.appendChild(ulPost)
 })
 
+/* setAtribute   para crear atributosd*/
 
-
-
-/* fetch("http://localhost:3000/posts")
-.then((res) => res.json())
-.then((data)=>{
-    const posts = data.map((post) => `<li>${post.userId} ${post.id} ${post.title} ${post.body}</li>`)
-    infoPosts.innerHTML = `<ul>${posts}</ul>`
-})
- */

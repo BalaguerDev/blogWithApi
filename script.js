2ce492d5a940663f83a8d27399a8a77acfc1cb2d
Alex Balaguer <balaguer.dev@gmail.com>
2022-08-10 11:42:22 +0200

    posts

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const btnCat = document.getElementById('btnCat');
-const catContainer = document.getElementById('catContainer');
 const infoPosts = document.getElementById("infoPosts")
-
+const ulPost = document.createElement('ul')
 
 fetch("http://localhost:3000/posts")
+.then((res)=> res.json())
+.then((posts)=>{
+    posts.forEach(post => {
+        /* crear elementos */
+        let listPost = document.createElement("li")
+
+        let titlePost = document.createElement("h2")
+        titlePost.appendChild(
+            document.createTextNode(`${post.title}`)
+        )
+
+        let bodyPost = document.createElement("p")
+        bodyPost.appendChild(
+            document.createTextNode(`${post.body}`)
+        )
+        
+        /* asignar elementos a sus padres */
+        listPost.appendChild(bodyPost)
+        listPost.appendChild(titlePost)
+        ulPost.appendChild(listPost)
+    });
+    infoPosts.appendChild(ulPost)
+})
+
+
+
+
+/* fetch("http://localhost:3000/posts")
 .then((res) => res.json())
 .then((data)=>{
     const posts = data.map((post) => `<li>${post.userId} ${post.id} ${post.title} ${post.body}</li>`)
     infoPosts.innerHTML = `<ul>${posts}</ul>`
 })
+ */

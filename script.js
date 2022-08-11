a7efd8bd88cd019e06a1e1ba605b3d3076627c38
Alex Balaguer <balaguer.dev@gmail.com>
2022-08-11 16:12:51 +0200

    end

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let contain = document.querySelector(".container")
 let url = "http://localhost:3000"
-let addHTML
+let modalHTML
+
 
 
 fetch(`${url}/posts`)
@@ -10,61 +11,84 @@ fetch(`${url}/posts`)
 
 
 /* creating elements and add attributes */
-let divP = document.createElement("div")
-    divP.setAttribute("data-bs-toggle", "modal")
-    divP.setAttribute("data-bs-target", `#exampleModal-${post.id}`)
+    let divP = document.createElement("div")
+        divP.setAttribute("data-bs-toggle", "modal")
+        divP.setAttribute("data-bs-target", `#exampleModal-${post.id}`)
 
-let titleP = document.createElement("h2")
-    titleP.setAttribute("class", "postTitle")
+        /* divP.setAttribute("data-bs-target", `#exampleModal-${post.id}`) */
 
-let bodyP = document.createElement("p")
-    bodyP.setAttribute("class", "postBody") 
+    let titleP = document.createElement("h3")
+        titleP.setAttribute("class", "postTitle")
 
-let separationP = document.createElement("hr") 
+    let bodyP = document.createElement("p")
+        bodyP.setAttribute("class", "postBody") 
+
+    let separationP = document.createElement("hr") 
 
 
 /* asign the API text to the variable */
-    titleP.appendChild(document.createTextNode(`${post.title}`))
-    bodyP.appendChild(document.createTextNode(`${post.body}`))
-    
+        titleP.appendChild(document.createTextNode(`${post.title}`))
+        bodyP.appendChild(document.createTextNode(`${post.body}`))
+        
 
 /* introducing children to the parents */
-    divP.appendChild(titleP)
-    divP.appendChild(bodyP)
-    divP.appendChild(separationP)
-    contain.appendChild(divP)
+        divP.appendChild(titleP)
+        divP.appendChild(separationP)
+        contain.appendChild(divP)
 
-    addHTML= `
+            fetch(`${url}/comments`)
+            .then(response => response.json())
+            .then(comments => {
+                comments.forEach(comment => {
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal-${post.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog ">
-        <div class="modal-content ">
+                fetch(`${url}/users`)
+                .then(response => response.json())
+                .then(users => {
+                    users.forEach(user => {
+                        if(post.userId === user.id){
+                            modalHTML= `
+                            <div class="modal fade" id="exampleModal-${post.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
 
-          <div class="modal-header ">
-            <h2 class="modal-title" id="exampleModalLabel">${post.title}</h2>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="exampleModalLabel">${post.title}</h4>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
 
-          <div class="modal-body">
-          ${post.body}
-    
-          <h3>Users</h3>
-          <p>alex123</p>
-          <p>mail</p>
-          </div>
+                                        <div class="modal-body">${post.body}
+                                            <h5>Users</h5>
+                                            <p>${user.username}</p>
+                                            <p>${user.email}</p>
+                                        </div>
 
-          <div class="modal-footer">
-          <h3>Users
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
+                                        <div class="modal-footer commentsSection">
+                                            <h3>Comments</h3>
+                                            <p>
+                                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+                                                Toggle width collapse
+                                            </button>
+                                            </p>
+                                            <div style="min-height: 120px;">
+                                                <div class="collapse collapse-horizontal" id="collapseWidthExample">
+                                                    <div class="card card-body" style="width: 300px;">
+                                                        <p>${comment.body}</p>
+                                                        <p>${comment.email}</p>
+                                                        <p>${comment.name}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
 
-        </div>
-      </div>
-    </div>
-    `
-    divP.insertAdjacentHTML("afterbegin",addHTML)
+                            divP.insertAdjacentHTML("afterbegin",modalHTML)
+                        }
+                        
+                    })
+                })
+            })
+        })
     })
-    
 })

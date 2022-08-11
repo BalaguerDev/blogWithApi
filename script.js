8550c7506caed3e175656dc9fee131d46b50f91f
Alex Balaguer <balaguer.dev@gmail.com>
2022-08-11 11:27:57 +0200

    push2

--- script.js
+++ script.js
@@ -40,17 +40,26 @@ let separationP = document.createElement("hr")
     <div class="modal fade" id="exampleModal-${post.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog ">
         <div class="modal-content ">
+
           <div class="modal-header ">
-            <h5 class="modal-title" id="exampleModalLabel">${post.title}</h5>
+            <h2 class="modal-title" id="exampleModalLabel">${post.title}</h2>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
+
           <div class="modal-body">
           ${post.body}
+    
+          <h3>Users</h3>
+          <p>alex123</p>
+          <p>mail</p>
           </div>
+
           <div class="modal-footer">
+          <h3>Users
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button type="button" class="btn btn-primary">Save changes</button>
           </div>
+
         </div>
       </div>
     </div>
